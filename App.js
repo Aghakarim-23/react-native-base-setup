afdf2d26ca04e59896b902edf77ce4c90d80f7c1
Aghakarim-23 <hemidzade1010@gmail.com>
2026-04-25 01:53:00 +0400

    add: simple login page

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import "./global.css";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Pressable } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-blue-500 items-center justify-center">
-      <Text className="text-white text-2xl font-bold">
-        NativeWind işləyir!
-      </Text>
+    <View className="flex-1 bg-gray-100 justify-center px-6">
+      {/* Header */}
+      <View className="mb-8">
+        <Text className="text-2xl font-bold text-gray-800">Login </Text>
+      </View>
+      {/* Email Input */}
+      <View className="mb-4">
+        <Text className="mb-1">
+          Email
+        </Text>
+        <TextInput
+          className="border border-gray-300 rounded-xl px-4 py-3"
+          placeholder="Enter your email..."
+        />
+      </View>
+
+      {/* Password Input */}
+      <View className="mb-4">
+        <Text className="mb-1">
+          Password
+        </Text>
+        <TextInput
+          className="border border-gray-300 rounded-xl px-4 py-3"
+          placeholder="Enter your password..."
+          secureTextEntry
+        />
+      </View>
+
+      {/* Button */}
+      <Pressable className="bg-blue-500 py-4 rounded-xl">
+        <Text className="text-white text-center font-semibold">
+          Login
+        </Text>
+      </Pressable>
     </View>
   );
 }
